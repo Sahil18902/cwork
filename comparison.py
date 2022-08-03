7781e0072fcaa17e0a31de04a89e347ff1458382
Sahil18902 <91956204+Sahil18902@users.noreply.github.com>
2022-08-03 10:24:23 +0530

    Add files via upload

---
+++ comparison.py
@@ -0,0 +1,9 @@
+# comparison operators
+a=5
+b=4
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
---
+++ exception.py
@@ -0,0 +1,28 @@
+# try, except,else,raise,finally
+
+# def concat():
+#     try:
+#         print(10+"abc")
+#     except BaseException as b:
+#         print("Unable to concatenate integer and string")
+#     finally:
+#         print("abc")
+
+# concat()
+
+
+def A():
+    try:
+        a=10
+        c=a/0
+        print(c)
+    except ZeroDivisionError:
+        print("Error")
+    else:
+        print(a)
+A()
+
+
+# x=-1
+# if x<0:
+#     raise Exception("Invalid")
